a1165a1adaf3eb105860c67cf27594ad9471029f
Mark <lookandlearn76@yahoo.co.uk>
2017-06-07 04:04:34 +0100

    added comments

--- app/assets/js/chat.js
+++ app/assets/js/chat.js
@@ -1,8 +1,8 @@
 $("document").ready(function(){
   $("#text-enter").keypress(function(e){
-    //Enter = 13
+//Enter = 13
     if(e.which == 13) {
-      //make sure page doesnt refresh
+//make sure page doesnt refresh
       e.preventDefault();
       $("#send").click();
     }
@@ -10,11 +10,11 @@ $("document").ready(function(){
   $("#send").click(function(event){
     event.preventDefault();
     var newMsg = $("#text-enter").val();
-    //store users name to be used later
+//store users name to be used later
     var userName = sessionStorage.getItem("userName");
-    //clear to receive next message
+//clear to receive next message
     $("#text-enter").val("");
-    //create nodes to append
+//create nodes to append
     var node1 = document.createElement("div");
     var node2 = document.createElement("div");
     var textNode1 = document.createTextNode(userName);
@@ -26,7 +26,7 @@ $("document").ready(function(){
     node2.appendChild(textNode2);
     $("#chat-box").append(node1);
     $("#chat-box").append(node2);
-    //make sure last message is seen at bottom
+//make sure last message is seen at bottom
     $("#chat-box").scrollTop($("#chat-box").prop("scrollHeight"));
   })
 });
@@ -34,7 +34,6 @@ $("document").ready(function(){
 
 function save() {
   var nameValue = document.getElementById("Name").value;
-  //save username to sessionStorage so it can be used in chat labels
+//save username to sessionStorage so it can be used in chat labels
   sessionStorage.setItem('userName', nameValue);
-
 };
